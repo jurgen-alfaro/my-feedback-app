2238633e25c099c41f35019c664876a7a48e82a0
jurgen-alfaro <dev.jurgen.alfaro@gmail.com>
2022-01-23 17:05:31 -0600

    Events & Prop Drilling

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,26 +1,28 @@
-import { useState } from "react";
+import { FaTimes } from "react-icons/fa";
 import Card from "../shared/Card";
 import PropTypes from "prop-types";
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item, handleDelete }) {
+  /*  
   const [rating, setRating] = useState(7);
   const [text, setText] = useState("This is an example of a feedback item");
 
   const handleClick = () => {
     setRating(10);
 
-    /*  
+    
     setRating((prev) => {
       return prev + 1;
     }) 
     
     You might want to to call a function this way if you want to access the previous value the component´s state had
     */
-  };
-
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
+      <button className='close' onClick={() => handleDelete(item.id)}>
+        <FaTimes color='purple' />
+      </button>
       <div className='text-display'>{item.text}</div>
       {/* <button onClick={handleClick}>Click</button> */}
     </Card>
